53cce19977beb032c7834220ee6f3e5d97aabffd
Gabriel Nascimento <gabrielnascimento.grn@gmail.com>
2024-08-31 15:55:05 -0300

    feat: add FeignClient to application

--- src/main/java/com/gabrielrnascimento/picpaydesafiobackend/PicpayDesafioBackendApplication.java
+++ src/main/java/com/gabrielrnascimento/picpaydesafiobackend/PicpayDesafioBackendApplication.java
@@ -2,12 +2,15 @@ package com.gabrielrnascimento.picpaydesafiobackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
 
 @SpringBootApplication
+@EnableFeignClients
 public class PicpayDesafioBackendApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PicpayDesafioBackendApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(PicpayDesafioBackendApplication.class, args);
+    }
 
 }
